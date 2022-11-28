9b0b55a4de7f7bb8bae9b51780ac1bf591619e53
Jasti  Krishna <jkrishna@ariqt.com>
2022-11-28 11:39:12 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
+import { UserListComponent } from './user-list/user-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'user', loadComponent: () => import('./user/user.component').then(m => m.UserComponent)
+    path: 'user', loadComponent: () => import('./user/user.component').then(m => m.UserComponent),
+    children: [
+      {
+        path: 'list', loadComponent: () => import('./user-list/user-list.component').then(m => m.UserListComponent)
+      },
+      {
+        path: '', redirectTo: 'list', pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '', redirectTo: 'user', pathMatch: 'full'
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,16 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { UserListComponent } from '../user-list/user-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '', loadChildren: () => import('../user-list/user-list.component').then(m => m.UserListComponent)
-  }
-];
-
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, UserListComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
